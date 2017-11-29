4215935a36e3bee34884976a4f7234325b491d2a
eamonwalshgmit <eamon.walsh@gmit.ie>
2017-11-29 19:29:40 +0000

    Draw more objects
    add a circle and change the square to rectangle

--- js/breakout.js
+++ js/breakout.js
@@ -2,7 +2,13 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(20, 40, 100, 100);
+ctx.rect(20, 40, 150, 50);
 ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
